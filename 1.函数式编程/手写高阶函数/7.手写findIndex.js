f3e6b80d216186604cc36e572d3c3795b3e41dcf
zmplly <1776321381@qq.com>
2021-07-26 22:24:45 +0800

    手写findIndex

---
+++ 1.函数式编程/手写高阶函数/7.手写findIndex.js
@@ -0,0 +1,33 @@
+
+Array.prototype.myFindIndex = function(fn,thisArg) {
+  if(Object.prototype.toString.call(fn) !== '[object Function]') {
+    throw new TypeError(`${fn} is not a function`)
+  }
+  let T;
+  if(thisArg) {
+    T = thisArg;
+  }
+  let O = Object(this);
+  let len = O.length >>> 0;
+  // 默认返回结果为-1,找不到就返回-1
+  let k = 0,result = -1;
+  while(k < len) {
+    if(k in O) {
+      if(fn.call(T,O[k],k,O)){
+        // 返回第一个索引
+        result = k;
+        // 找到第一个满足条件的终止
+        break;
+      }
+    }
+    k++
+  }
+  return result
+}
+
+let arr = [1,2,3,4,5,6,7,7,8];
+
+let res = arr.myFindIndex((ele,i,arr)=>{
+  return ele > 10
+})
+console.log('第一个大于5的索引为',res);
