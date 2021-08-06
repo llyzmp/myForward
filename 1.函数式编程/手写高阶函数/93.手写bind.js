fea72219e673a7962bf71445d10bea574ce77b41
zmplly <1776321381@qq.com>
2021-08-06 16:21:44 +0800

    手写bind

---
+++ 1.函数式编程/手写高阶函数/93.手写bind.js
@@ -0,0 +1,25 @@
+/**
+ * 实现bind
+ * 语法： function.bind(thisArg[, arg1[, arg2[, ...]]])
+ * @param {*} thisArg 
+ * @param  {...any} args 
+ * 返回值： 返回一个原函数的拷贝，并拥有指定的this值和初始参数
+ */
+Function.prototype.myBind = function(thisArg) {
+  if(thisArg === undefined || thisArg === null) {
+    thisArg = window;
+  }else {
+    thisArg = Object(this);
+  }
+  // 保存一下this
+  let _this = this;
+  // 获取参数
+  let arg1 = Array.from(arguments).slice(1);
+  function F() {
+    let arg2 = Array.from(arguments);
+    return _this.apply(this instanceof F ? this /*作为构造函数使用 */ : thisArg.arg1.contract(arg2))
+  }
+  // 作为构造函数使用,后续通过new创建实例对象,要可以使用原型上的方法
+  F.prototype = Object.create(this.prototype);
+  return F
+}
